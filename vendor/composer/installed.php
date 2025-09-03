634b2eb36c7271ecc8dc6fe39613e0dc2d2a07f0
JoggingSplash <santisegura15@gmail.com>
2025-09-03 11:09:57 -0300

    composer stuff

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'bitpvp/text-packet-interceptor',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5572d92559d3dda010d87dcbb3fe600a387030c1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'bitpvp/text-packet-interceptor' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5572d92559d3dda010d87dcbb3fe600a387030c1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
